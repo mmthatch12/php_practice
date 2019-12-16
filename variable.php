ac4c3bda973256fe3cca754c05c1ac4a9f1ae517
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-16 16:48:47 -0700

    first commit

---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php 
+// variables
+// start with $ $name
+// can have dashes(-)
+// underscore(_)
+// letters
+// numbers
+// case sensitive
+// cannot have spaces
+// can't start with numbers
+// bad practice to start variable with - or _
+
+
+echo "is this thing on?"
+?>
