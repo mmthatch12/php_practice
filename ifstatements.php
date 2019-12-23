9722bbcae723f311513f47997131eac0ab102c96
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-23 14:40:16 -0700

    working on ifstatements

---
+++ ifstatements.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>If Statement</title>
+    </head>
+    <body>
+        <h1>Yo Bio!<h1>
+<?php
+$Weather = "Cloudy";
+if($Weather=="Sunny"){
+    echo "Weather is pleasant<br>";
+} elseif($Weather =="Cloudy"){
+    echo "Weather is Cloudy<br>";
+} else{
+    echo "Weather is NEITHER PLESANT NOR CLOUDY!<br>";
+}
+?><br>
+<?php  echo 2+3?>
+    </body>
+</html>
