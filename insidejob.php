9d5f81e408789ff86fc324444e451977896650d5
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-23 14:20:42 -0700

    insidejob

---
+++ insidejob.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Inside Job</title>
+    </head>
+    <body>
+        <h1>Yo Bio!<h1>
+<?php
+// hello
+echo "Do you see this?"; 
+?><br>
+<?php 
+$numbers = array(2,6,7,20,85)
+?>
+<?php echo $numbers[3]?><br>
+    </body>
+</html>
