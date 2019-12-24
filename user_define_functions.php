948d6a62ef6254456462654cfe730d98139d0c10
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-24 14:04:12 -0700

    finished user_define_functions.php

---
+++ user_define_functions.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>User Define Functions</title>
+    </head>
+    <body>
+        <h1>Yo Bio!<h1>
+<?php
+function adds($a, $b){
+    return $a+$b;
+};
+
+?><br>
+<?php  echo adds(2, 5);?>
+    </body>
+</html>
